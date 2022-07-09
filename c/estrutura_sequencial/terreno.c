78398b3c01b1f3e09902039de3f42e6de6a270fb
anaalucca <107819322+anaalucca@users.noreply.github.com>
2022-07-09 02:01:37 -0300

    Create terreno.c

---
+++ c/estrutura_sequencial/terreno.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double largura, comprimento, valor, area, preco;
+    
+    printf("Digite a largura do terreno: ");
+    scanf("%lf", &largura);
+    
+    printf("Digite o comprimento do terreno: ");
+    scanf("%lf", &comprimento);
+    
+    printf("Digite o valor do metro quadrado: ");
+    scanf("%lf", &valor);
+    
+    area = largura * comprimento;
+    preco = area * valor;
+    
+    printf("Area do terreno = %.2lf\n", area);
+    printf("Preco do terreno = %.2lf\n", preco);
+    
+}
