400036d8f8b5ff5f75614d1b316096711dc97413
anaalucca <107819322+anaalucca@users.noreply.github.com>
2022-07-09 01:46:18 -0300

    Create crescente.py

---
+++ python/crescente.py
@@ -0,0 +1,13 @@
+print("Digite dois numeros: ")
+numero1 = int(input())
+numero2 = int(input())
+
+while numero1 != numero2:
+    if numero1 < numero2:
+        print("CRESCENTE!")
+    else:
+        print("DECRESCENTE!")
+        
+print("Digite outros dois numeros: ")
+numero1 = int(input())
+numero2 = int(input())
