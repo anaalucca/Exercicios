4ad469844e05a317800a519981dcbcd416f73669
anaalucca <107819322+anaalucca@users.noreply.github.com>
2022-07-10 16:50:02 -0300

    Create quadrante.c

---
+++ c/estrutura_enquanto/quadrante.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x, y; 
+    
+    printf("Digite os valores das coordenadas X e Y:\n");
+    scanf("%i", &x); 
+    scanf("%i", &y);
+    
+    while (x != 0 && y != 0)
+    {
+        if (x > 0 && y > 0)
+        {
+            printf("QUADRANTE Q1\n");
+        }
+        else if (x < 0 && y > 0)
+        {
+            printf("QUADRANTE Q2\n");
+        }
+        else if (x < 0 && y < 0)
+        {
+            printf("QUADRANTE Q3\n");
+        }
+        else {
+            printf("QUADRANTE Q4\n");
+        }
+    }
+    
+    printf("Digite os valores das coordenadas X e Y:\n");
+    scanf("%i", &x); 
+    scanf("%i", &y);
+    
+}
