56f10d26dd4d6477aef723c09705c022905d57fc
anaalucca <107819322+anaalucca@users.noreply.github.com>
2022-07-10 22:11:53 -0300

    Create temperatura.py

---
+++ python/estrutura_condicional/temperatura.py
@@ -0,0 +1,12 @@
+escala = input("Voce vai digitar a temperatura em qual escala (C/F)? ")
+
+if escala == 'F':
+    fahrenheit = float(input(f"Digite a temperatura em Fahrenheit: "))
+    
+    celsius = 5.0 / 9.0 * (fahrenheit - 32.0)
+    print(f"Temperatura equivalente em Celsius: {celsius:.2f}")
+else:
+    celsius = float(input("Digite a temperatura em Celsius: "))
+    
+    fahrenheit = celsius * 9.0 / 5.0 + 32.0
+    print(f"Temperatura equivalente em Fahrenheit: {fahrenheit:.2f}")    
